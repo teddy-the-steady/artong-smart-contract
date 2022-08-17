4ddf65b43527e0f8e4aa8ff4ee92ce1592cfac89
teddy <teddy.jeon@speedwagon.io>
2022-08-17 11:21:14 +0900

    allowUnlimitedContractSize added to hardhat.config, deploy script added

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,4 +5,19 @@ require('@openzeppelin/hardhat-upgrades');
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true
+    },
+    // mainnet: {
+    //   url: `https://rpcapi.fantom.network`,
+    //   chainId: 250,
+    //   accounts: [`0x${PRIVATE_KEY}`]
+    // },
+    // testnet: {
+    //   url: `https://rpcapi-tracing.testnet.fantom.network`,
+    //   chainId: 4002,
+    //   accounts: [`0x${PRIVATE_KEY}`]
+    // },
+  },
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,37 @@
+const constants = require('./constants');
+const { ethers, upgrades } = require('hardhat');
+const fs = require('fs');
+
+async function main() {
+  const Marketplace = await ethers.getContractFactory('ArtongMarketplace');
+  console.log('Deploying Marketplace...');
+  const marketplace = await upgrades.deployProxy(
+    Marketplace,
+    [constants.PLATFORM_FEE, constants.TREASURY_ADDRESS],
+    { initializer: 'initialize' }
+  );
+  await marketplace.deployed();
+  console.log('Marketplace deployed to:', marketplace.address);
+
+  const Factory = await ethers.getContractFactory('ArtongNFTFactory');
+  console.log('Deploying Factory...');
+  const factory = await Factory.deploy(
+    marketplace.address,
+    constants.TREASURY_ADDRESS,
+    constants.PLATFORM_FEE
+  );
+  await factory.deployed();
+  console.log('Factory deployed to:', factory.address);
+
+  constants['MARKETPLACE'] = marketplace.address;
+  constants['FACTORY'] = factory.address;
+    
+  fs.writeFileSync('scripts/constants.js', `module.exports = ${JSON.stringify(constants, null, "\t")}`)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
