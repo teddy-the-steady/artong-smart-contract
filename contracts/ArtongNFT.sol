f90c5492309e6456112b7194f0223c07fb291bcf
teddy <teddy.jeon@speedwagon.io>
2022-09-30 10:17:57 +0900

    added ERC721Enumerable to ArtongNFT.sol, ropsten created new address

--- contracts/ArtongNFT.sol
+++ contracts/ArtongNFT.sol
@@ -9,6 +9,7 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "./Enums.sol";
 
 interface IArtongMarketplace {
@@ -22,7 +23,8 @@ contract ArtongNFT is
     EIP712,
     Pausable,
     ERC721Burnable,
-    Ownable
+    Ownable,
+    ERC721Enumerable
 {
     using Counters for Counters.Counter;
 
@@ -204,6 +206,22 @@ contract ArtongNFT is
             voucher.minPrice,
             keccak256(bytes(voucher.uri))
         )));
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
 
