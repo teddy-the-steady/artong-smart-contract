bf79e74aa4e09b02f63ca1f998c34abc98be24c6
teddy <teddy.jeon@speedwagon.io>
2023-02-20 11:11:51 +0900

    testnet branch created

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,21 +22,21 @@ module.exports = {
       allowUnlimitedContractSize: true,
       chainId: 1337
     },
-    // goerli: {
-    //   url: 'https://goerli.infura.io/v3/c60789555fff407eabc1c2bfa1330684',
-    //   accounts: [
-    //     process.env.PRIVATE_KEY_2,
-    //     process.env.PRIVATE_KEY_1,
-    //     process.env.PRIVATE_KEY_3,
-    //     process.env.PRIVATE_KEY_4,
-    //     process.env.PRIVATE_KEY_5,
-    //   ],
-    //   gas: 2100000,
-    //   gasPrice: 8000000000,
-    // },
-    mainnet: {
-      url: 'https://mainnet.infura.io/v3/ae3e3f39ab144c7b9a0ef19d3ff80aa9',
-      accounts: [process.env.MAINNET_PRIVATE_KEY]
+    goerli: {
+      url: 'https://goerli.infura.io/v3/c60789555fff407eabc1c2bfa1330684',
+      accounts: [
+        process.env.PRIVATE_KEY_2,
+        process.env.PRIVATE_KEY_1,
+        process.env.PRIVATE_KEY_3,
+        process.env.PRIVATE_KEY_4,
+        process.env.PRIVATE_KEY_5,
+      ],
+      gas: 2100000,
+      gasPrice: 8000000000,
     },
+    // mainnet: {
+    //   url: 'https://mainnet.infura.io/v3/ae3e3f39ab144c7b9a0ef19d3ff80aa9',
+    //   accounts: [process.env.MAINNET_PRIVATE_KEY]
+    // },
   },
 };
